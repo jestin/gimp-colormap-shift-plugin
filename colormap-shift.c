e5f1b053c5fec95e6744256b681407c7391f07ff
Jestin Stoffel <jestin.stoffel@gmail.com>
2022-09-04 16:37:41 -0500

    Fix indentation

--- colormap-shift.c
+++ colormap-shift.c
@@ -38,13 +38,13 @@
 
 
 /* Declare local functions.
- */
+*/
 static void       query        (void);
 static void       run          (const gchar      *name,
-                                gint              nparams,
-                                const GimpParam  *param,
-                                gint             *nreturn_vals,
-                                GimpParam       **return_vals);
+		gint              nparams,
+		const GimpParam  *param,
+		gint             *nreturn_vals,
+		GimpParam       **return_vals);
 
 static void shift_color_map(guchar* orig,
 		guchar** shifted,
@@ -52,15 +52,15 @@ static void shift_color_map(guchar* orig,
 		gint offset);
 
 static gboolean   shift_dialog (gint32            image_ID,
-                                guchar           *map);
+		guchar           *map);
 
 
 const GimpPlugInInfo PLUG_IN_INFO =
 {
-  NULL,  /* init_proc  */
-  NULL,  /* quit_proc  */
-  query, /* query_proc */
-  run,   /* run_proc   */
+	NULL,  /* init_proc  */
+	NULL,  /* quit_proc  */
+	query, /* query_proc */
+	run,   /* run_proc   */
 };
 
 typedef struct
@@ -76,8 +76,7 @@ static ColorShiftVals colorshiftvals = defaults;
 
 MAIN ()
 
-static void
-query (void)
+static void query (void)
 {
 	static const GimpParamDef shift_args[] =
 	{
@@ -107,12 +106,11 @@ query (void)
 			(const guint8 *) GIMP_ICON_COLORMAP);
 }
 
-static void
-run (const gchar      *name,
-     gint              nparams,
-     const GimpParam  *param,
-     gint             *nreturn_vals,
-	 GimpParam       **return_vals)
+static void run (const gchar      *name,
+		gint              nparams,
+		const GimpParam  *param,
+		gint             *nreturn_vals,
+		GimpParam       **return_vals)
 {
 	static GimpParam   values[1];
 	gint32             image_ID;
@@ -207,10 +205,10 @@ run (const gchar      *name,
 
 enum
 {
-  COLOR_INDEX,
-  COLOR_INDEX_TEXT,
-  COLOR_RGB,
-  NUM_COLS
+	COLOR_INDEX,
+	COLOR_INDEX_TEXT,
+	COLOR_RGB,
+	NUM_COLS
 };
 
 static  GtkUIManager *shift_ui  = NULL;
@@ -239,237 +237,231 @@ static void shift_color_map(guchar* orig,
 	}
 }
 
-static void
-shift_reset_callback (GtkAction       *action,
-                      GtkTreeSortable *store)
+static void shift_reset_callback (GtkAction       *action,
+		GtkTreeSortable *store)
 {
 }
 
-static GtkUIManager *
-shift_ui_manager_new (GtkWidget    *window,
-                      GtkListStore *store)
+static GtkUIManager * shift_ui_manager_new (GtkWidget    *window,
+		GtkListStore *store)
 {
-  static const GtkActionEntry actions[] =
-  {
-    {
-      "reset", GIMP_ICON_RESET, "Reset Order", NULL, NULL,
-      G_CALLBACK (shift_reset_callback)
-    },
-  };
+	static const GtkActionEntry actions[] =
+	{
+		{
+			"reset", GIMP_ICON_RESET, "Reset Order", NULL, NULL,
+			G_CALLBACK (shift_reset_callback)
+		},
+	};
 
-  GtkUIManager   *ui_manager = gtk_ui_manager_new ();
-  GtkActionGroup *group      = gtk_action_group_new ("Actions");
-  GError         *error      = NULL;
+	GtkUIManager   *ui_manager = gtk_ui_manager_new ();
+	GtkActionGroup *group      = gtk_action_group_new ("Actions");
+	GError         *error      = NULL;
 
-  gtk_action_group_set_translation_domain (group, NULL);
-  gtk_action_group_add_actions (group, actions, G_N_ELEMENTS (actions), store);
+	gtk_action_group_set_translation_domain (group, NULL);
+	gtk_action_group_add_actions (group, actions, G_N_ELEMENTS (actions), store);
 
-  gtk_ui_manager_insert_action_group (ui_manager, group, -1);
-  g_object_unref (group);
+	gtk_ui_manager_insert_action_group (ui_manager, group, -1);
+	g_object_unref (group);
 
-  gtk_ui_manager_add_ui_from_string (ui_manager,
-                                     "<ui>"
-                                     "  <popup name=\"shift-popup\">"
-                                     "    <menuitem action=\"reset\" />"
-                                     "  </popup>"
-                                     "</ui>",
-                                     -1, &error);
-  if (error)
-    {
-      g_warning ("error parsing ui: %s", error->message);
-      g_clear_error (&error);
-    }
+	gtk_ui_manager_add_ui_from_string (ui_manager,
+			"<ui>"
+			"  <popup name=\"shift-popup\">"
+			"    <menuitem action=\"reset\" />"
+			"  </popup>"
+			"</ui>",
+			-1, &error);
+	if (error)
+	{
+		g_warning ("error parsing ui: %s", error->message);
+		g_clear_error (&error);
+	}
 
-  return ui_manager;
+	return ui_manager;
 }
 
-static gboolean
-shift_popup_menu (GtkWidget      *widget,
-                  GdkEventButton *event)
+static gboolean shift_popup_menu (GtkWidget      *widget,
+		GdkEventButton *event)
 {
-  GtkWidget *menu = gtk_ui_manager_get_widget (shift_ui, "/shift-popup");
+	GtkWidget *menu = gtk_ui_manager_get_widget (shift_ui, "/shift-popup");
 
-  gtk_menu_set_screen (GTK_MENU (menu), gtk_widget_get_screen (widget));
-  gtk_menu_popup (GTK_MENU (menu),
-                  NULL, NULL, NULL, NULL,
-                  event ? event->button : 0,
-                  event ? event->time   : gtk_get_current_event_time ());
+	gtk_menu_set_screen (GTK_MENU (menu), gtk_widget_get_screen (widget));
+	gtk_menu_popup (GTK_MENU (menu),
+			NULL, NULL, NULL, NULL,
+			event ? event->button : 0,
+			event ? event->time   : gtk_get_current_event_time ());
 
-  return TRUE;
+	return TRUE;
 }
 
-static gboolean
-shift_button_press (GtkWidget      *widget,
-                    GdkEventButton *event)
+static gboolean shift_button_press (GtkWidget      *widget,
+		GdkEventButton *event)
 {
-  if (gdk_event_triggers_context_menu ((GdkEvent *) event))
-    return shift_popup_menu (widget, event);
+	if (gdk_event_triggers_context_menu ((GdkEvent *) event))
+		return shift_popup_menu (widget, event);
 
-  return FALSE;
+	return FALSE;
 }
 
-static void
-shift_response (GtkWidget       *dialog,
-                gint             response_id,
-                GtkTreeSortable *store)
+static void shift_response (GtkWidget       *dialog,
+		gint             response_id,
+		GtkTreeSortable *store)
 {
-  switch (response_id)
-    {
-    case RESPONSE_RESET:
-      shift_reset_callback (NULL, store);
-      break;
+	switch (response_id)
+	{
+		case RESPONSE_RESET:
+			shift_reset_callback (NULL, store);
+			break;
 
-    case GTK_RESPONSE_OK:
-      shift_run = TRUE;
-      /* fallthrough */
+		case GTK_RESPONSE_OK:
+			shift_run = TRUE;
+			/* fallthrough */
 
-    default:
-      gtk_main_quit ();
-      break;
-    }
+		default:
+			gtk_main_quit ();
+			break;
+	}
 }
 
-static gboolean
-shift_dialog (gint32  image_ID,
-              guchar *map)
+static gboolean shift_dialog (gint32  image_ID,
+		guchar *map)
 {
-  GtkWidget       *dialog;
-  GtkWidget       *vbox;
-  GtkWidget       *box;
-  GtkWidget       *iconview;
-  GtkListStore    *store;
-  GtkCellRenderer *renderer;
-  GtkTreeIter      iter;
-  guchar          *cmap;
-  gint             palsize, i;
-  gboolean         valid;
+	GtkWidget       *dialog;
+	GtkWidget       *vbox;
+	GtkWidget       *box;
+	GtkWidget       *iconview;
+	GtkListStore    *store;
+	GtkCellRenderer *renderer;
+	GtkTreeIter      iter;
+	guchar          *cmap;
+	gint             palsize, i;
+	gboolean         valid;
 
-  gimp_ui_init (PLUG_IN_BINARY, FALSE);
+	gimp_ui_init (PLUG_IN_BINARY, FALSE);
 
-  dialog = gimp_dialog_new ("Shift Colormap", PLUG_IN_ROLE,
-                            NULL, 0,
-                            gimp_standard_help_func, PLUG_IN_PROC_SHIFT,
+	dialog = gimp_dialog_new ("Shift Colormap", PLUG_IN_ROLE,
+			NULL, 0,
+			gimp_standard_help_func, PLUG_IN_PROC_SHIFT,
 
-                            "_Reset",  RESPONSE_RESET,
-                            "_Cancel", GTK_RESPONSE_CANCEL,
-                            "_OK",     GTK_RESPONSE_OK,
+			"_Reset",  RESPONSE_RESET,
+			"_Cancel", GTK_RESPONSE_CANCEL,
+			"_OK",     GTK_RESPONSE_OK,
 
-                            NULL);
+			NULL);
 
-  gtk_dialog_set_alternative_button_order (GTK_DIALOG (dialog),
-                                           RESPONSE_RESET,
-                                           GTK_RESPONSE_OK,
-                                           GTK_RESPONSE_CANCEL,
-                                           -1);
+	gtk_dialog_set_alternative_button_order (GTK_DIALOG (dialog),
+			RESPONSE_RESET,
+			GTK_RESPONSE_OK,
+			GTK_RESPONSE_CANCEL,
+			-1);
 
-  gimp_window_set_transient (GTK_WINDOW (dialog));
+	gimp_window_set_transient (GTK_WINDOW (dialog));
 
-  vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 12);
-  gtk_container_set_border_width (GTK_CONTAINER (vbox), 12);
-  gtk_box_pack_start (GTK_BOX (gtk_dialog_get_content_area (GTK_DIALOG (dialog))),
-                      vbox, TRUE, TRUE, 0);
+	vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 12);
+	gtk_container_set_border_width (GTK_CONTAINER (vbox), 12);
+	gtk_box_pack_start (GTK_BOX (gtk_dialog_get_content_area (GTK_DIALOG (dialog))),
+			vbox, TRUE, TRUE, 0);
 
-  cmap = gimp_image_get_colormap (image_ID, &palsize);
+	cmap = gimp_image_get_colormap (image_ID, &palsize);
 
-  g_return_val_if_fail ((palsize > 0) && (palsize <= 256), FALSE);
+	g_return_val_if_fail ((palsize > 0) && (palsize <= 256), FALSE);
 
-  store = gtk_list_store_new (NUM_COLS,
-                              G_TYPE_INT, G_TYPE_STRING, GIMP_TYPE_RGB,
-                              G_TYPE_DOUBLE, G_TYPE_DOUBLE, G_TYPE_DOUBLE);
+	store = gtk_list_store_new (NUM_COLS,
+			G_TYPE_INT, G_TYPE_STRING, GIMP_TYPE_RGB,
+			G_TYPE_DOUBLE, G_TYPE_DOUBLE, G_TYPE_DOUBLE);
 
-  for (i = 0; i < palsize; i++)
-    {
-      GimpRGB  rgb;
-      GimpHSV  hsv;
-      gint     index = map[i];
+	for (i = 0; i < palsize; i++)
+	{
+		GimpRGB  rgb;
+		GimpHSV  hsv;
+		gint     index = map[i];
 
-      gimp_rgb_set_uchar (&rgb,
-                          cmap[index * 3],
-                          cmap[index * 3 + 1],
-                          cmap[index * 3 + 2]);
-      gimp_rgb_to_hsv (&rgb, &hsv);
+		gimp_rgb_set_uchar (&rgb,
+				cmap[index * 3],
+				cmap[index * 3 + 1],
+				cmap[index * 3 + 2]);
+		gimp_rgb_to_hsv (&rgb, &hsv);
 
-      gtk_list_store_append (store, &iter);
-      gtk_list_store_set (store, &iter,
-                          COLOR_RGB,        &rgb,
-                          -1);
-    }
+		gtk_list_store_append (store, &iter);
+		gtk_list_store_set (store, &iter,
+				COLOR_RGB,        &rgb,
+				-1);
+	}
 
-  g_free (cmap);
+	g_free (cmap);
 
-  shift_ui = shift_ui_manager_new (dialog, store);
+	shift_ui = shift_ui_manager_new (dialog, store);
 
-  iconview = gtk_icon_view_new_with_model (GTK_TREE_MODEL (store));
-  g_object_unref (store);
+	iconview = gtk_icon_view_new_with_model (GTK_TREE_MODEL (store));
+	g_object_unref (store);
 
-  gtk_box_pack_start (GTK_BOX (vbox), iconview, TRUE, TRUE, 0);
+	gtk_box_pack_start (GTK_BOX (vbox), iconview, TRUE, TRUE, 0);
 
-  gtk_icon_view_set_selection_mode (GTK_ICON_VIEW (iconview),
-                                    GTK_SELECTION_SINGLE);
-  gtk_icon_view_set_orientation (GTK_ICON_VIEW (iconview),
-                                 GTK_ORIENTATION_VERTICAL);
-  gtk_icon_view_set_columns (GTK_ICON_VIEW (iconview), 16);
-  gtk_icon_view_set_row_spacing (GTK_ICON_VIEW (iconview), 0);
-  gtk_icon_view_set_column_spacing (GTK_ICON_VIEW (iconview), 0);
-  gtk_icon_view_set_reorderable (GTK_ICON_VIEW (iconview), FALSE);
+	gtk_icon_view_set_selection_mode (GTK_ICON_VIEW (iconview),
+			GTK_SELECTION_SINGLE);
+	gtk_icon_view_set_orientation (GTK_ICON_VIEW (iconview),
+			GTK_ORIENTATION_VERTICAL);
+	gtk_icon_view_set_columns (GTK_ICON_VIEW (iconview), 16);
+	gtk_icon_view_set_row_spacing (GTK_ICON_VIEW (iconview), 0);
+	gtk_icon_view_set_column_spacing (GTK_ICON_VIEW (iconview), 0);
+	gtk_icon_view_set_reorderable (GTK_ICON_VIEW (iconview), FALSE);
 
-  renderer = gimp_cell_renderer_color_new ();
-  gtk_cell_layout_pack_start (GTK_CELL_LAYOUT (iconview), renderer, TRUE);
-  gtk_cell_layout_set_attributes (GTK_CELL_LAYOUT (iconview), renderer,
-                                  "color", COLOR_RGB,
-                                  NULL);
-  g_object_set (renderer,
-                "width", 0,
-                NULL);
+	renderer = gimp_cell_renderer_color_new ();
+	gtk_cell_layout_pack_start (GTK_CELL_LAYOUT (iconview), renderer, TRUE);
+	gtk_cell_layout_set_attributes (GTK_CELL_LAYOUT (iconview), renderer,
+			"color", COLOR_RGB,
+			NULL);
+	g_object_set (renderer,
+			"width", 0,
+			NULL);
 
-  renderer = gtk_cell_renderer_text_new ();
-  gtk_cell_layout_pack_start (GTK_CELL_LAYOUT (iconview), renderer, TRUE);
-  gtk_cell_layout_set_attributes (GTK_CELL_LAYOUT (iconview), renderer,
-                                  NULL);
-  g_object_set (renderer,
-                "size-points", 0.0,
-                "xalign",      0.0,
-                "ypad",        0,
-                NULL);
+	renderer = gtk_cell_renderer_text_new ();
+	gtk_cell_layout_pack_start (GTK_CELL_LAYOUT (iconview), renderer, TRUE);
+	gtk_cell_layout_set_attributes (GTK_CELL_LAYOUT (iconview), renderer,
+			NULL);
+	g_object_set (renderer,
+			"size-points", 0.0,
+			"xalign",      0.0,
+			"ypad",        0,
+			NULL);
 
-  g_signal_connect (iconview, "popup-menu",
-                    G_CALLBACK (shift_popup_menu),
-                    NULL);
+	g_signal_connect (iconview, "popup-menu",
+			G_CALLBACK (shift_popup_menu),
+			NULL);
 
-  g_signal_connect (iconview, "button-press-event",
-                    G_CALLBACK (shift_button_press),
-                    NULL);
+	g_signal_connect (iconview, "button-press-event",
+			G_CALLBACK (shift_button_press),
+			NULL);
 
-  box = gimp_hint_box_new (("Drag and drop colors to rearrange the colormap.  "
-                             "The numbers shown are the original indices.  "
-                             "Right-click for a menu with sort options."));
+	box = gimp_hint_box_new (("Drag and drop colors to rearrange the colormap.  "
+				"The numbers shown are the original indices.  "
+				"Right-click for a menu with sort options."));
 
-  gtk_box_pack_start (GTK_BOX (vbox), box, FALSE, FALSE, 0);
-  gtk_widget_show (box);
+	gtk_box_pack_start (GTK_BOX (vbox), box, FALSE, FALSE, 0);
+	gtk_widget_show (box);
 
-  g_signal_connect (dialog, "response",
-                    G_CALLBACK (shift_response),
-                    store);
+	g_signal_connect (dialog, "response",
+			G_CALLBACK (shift_response),
+			store);
 
-  gtk_widget_show_all (dialog);
+	gtk_widget_show_all (dialog);
 
-  gtk_main ();
+	gtk_main ();
 
-  i = 0;
+	i = 0;
 
-  for (valid = gtk_tree_model_get_iter_first (GTK_TREE_MODEL (store), &iter);
-       valid;
-       valid = gtk_tree_model_iter_next (GTK_TREE_MODEL (store), &iter))
-    {
-      gint index;
+	for (valid = gtk_tree_model_get_iter_first (GTK_TREE_MODEL (store), &iter);
+			valid;
+			valid = gtk_tree_model_iter_next (GTK_TREE_MODEL (store), &iter))
+	{
+		gint index;
 
-      gtk_tree_model_get (GTK_TREE_MODEL (store), &iter,
-                          COLOR_INDEX, &index,
-                          -1);
-      map[i++] = index;
-    }
+		gtk_tree_model_get (GTK_TREE_MODEL (store), &iter,
+				COLOR_INDEX, &index,
+				-1);
+		map[i++] = index;
+	}
 
-  gtk_widget_destroy (dialog);
+	gtk_widget_destroy (dialog);
 
-  return shift_run;
+	return shift_run;
 }
